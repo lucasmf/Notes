0fd2ce5175c92a3c4dc64d44ded68ec8051642c5
lucas <lucas.figueredo@ccc.ufcg.edu.br>
2014-10-23 13:41:28 -0300

    Exemplo de segment tree adicionado

---
+++ Templates/segmentTreeExample.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAXN 30010
+#define LEFT(p) (2*p)
+#define RIGHT(p) (2*p+1)
+#define MID(a, b) ((a+b)/2)
+
+struct node {
+	int sum;
+	int minimum;
+	node() {}
+	node(char c) {
+		sum = (c=='(' ? 1 : -1);
+		minimum = min(0, sum);
+	}
+	node(int a, int b) {
+		sum = a;
+		minimum = b;
+	}
+};
+	
+node st[4*MAXN];
+char word[MAXN];
+
+node join(node left, node right) {
+	int sum = left.sum + right.sum;
+	int minimum = min(left.minimum, right.minimum+left.sum);
+	return node(sum, minimum);
+}
+
+void build(int p, int beg, int end) {
+	if(beg > end) return;
+	if(beg == end) {
+		st[p] = node(word[beg]);
+		return;
+	}
+	build(LEFT(p), beg, MID(beg, end));
+	build(RIGHT(p), MID(beg, end)+1, end);
+	
+	st[p] = join(st[LEFT(p)], st[RIGHT(p)]);
+	
+}
+
+void update(int p, int idx, int beg, int end, char c) {
+	if(beg > end || beg > idx || end < idx) return;
+	if(beg == end) {
+		st[p] = node(c);
+		return;
+	}
+	update(LEFT(p), idx, beg, MID(beg, end), c);
+	update(RIGHT(p), idx, MID(beg, end)+1, end, c);
+	st[p] = join(st[LEFT(p)], st[RIGHT(p)]);
+}
+
+
+bool read(int p) {
+	return (st[p].minimum >= 0 && st[p].sum == 0);
+}
+
+int main() {
+	for(int test = 1; test<=10; test++) {
+		printf("Test %d:\n", test);
+		int n;
+		scanf("%d", &n);
+		scanf(" %s", word);
+		build(1, 0, n-1);
+		//printf("%d", read(1, 0, n-1));
+		int m;
+		scanf("%d", &m);
+		for(int i = 0; i<m; i++) {
+			int q;
+			scanf("%d", &q);
+			if(q == 0) {
+				printf("%s\n", read(1) == true?"YES":"NO"); 
+			}
+			else {
+				if(word[q-1] == ')') word[q-1] = '(';
+				else  word[q-1] = ')';
+				update(1, q-1, 0, n-1, word[q-1]);
+			}
+		}
+	}
+	return 0;
+}
+
+
