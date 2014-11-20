ebcdadcf023433800719955183bf66f49382b25a
Lucas <lucas.figueredo@ccc.ufcg.edu.br>
2014-11-20 12:57:21 -0300

    Problema C levando AC :)

--- WF2011/C.cpp
+++ WF2011/C.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 #define MAXN 210
 
-//Nao testado, submission failed
+//AC, finalmente!
 
 int grid[MAXN][MAXN];
 int color[MAXN][MAXN];
 set<int> adj[MAXN*MAXN];
 vector<int> blacks;
-int r[7];
+int r[8];
 map<char, string> mm;
 
 void add(int x, int y, char c) {
@@ -20,6 +20,18 @@ void add(int x, int y, char c) {
       grid[x][y+2] = str[2] - '0';
       grid[x][y+3] = str[3] - '0';
 }
+
+void dfs(int x, int y, int v) {
+	if(x < 0 || x >= MAXN || y < 0 || y >= MAXN) return;
+	if(color[x][y] != 0) return;
+	color[x][y] = v;
+	if(grid[x][y] == grid[x+1][y]) dfs(x+1, y, v);
+	if(grid[x][y] == grid[x-1][y]) dfs(x-1, y, v);
+	if(grid[x][y] == grid[x][y+1]) dfs(x, y+1, v);
+	if(grid[x][y] == grid[x][y-1]) dfs(x, y-1, v);
+
+}
+
 
 int main() {
       mm['0']="0000";
@@ -39,13 +51,14 @@ int main() {
       mm['e']="1110";
       mm['f']="1111";
       int teste = 1;
-      while(1) {
+      int h, w;
+      int idx;
+      while(scanf("%d %d", &h, &w) > 0 && h != 0){
 
-	    int h, w;
-	    scanf("%d %d", &h, &w);
+	   
 	    if(h == 0 || w == 0) break;
-   
-	    int idx = 1;
+		idx = 1;
+	    
 	    blacks.clear();
 	    for(int i = 0; i<MAXN; i++) {
 		  for(int j = 0; j<MAXN; j++) {
@@ -56,29 +69,34 @@ int main() {
 	    for(int i = 0; i<MAXN*MAXN; i++) {
 			adj[i].clear();
 		}
-	    for(int i = 0; i<7; i++) r[i] = 0;
+	    for(int i = 0; i<8; i++) r[i] = 0;
 
 	    for(int i = 1; i<=h; i++) {
-		  for(int j = 1; j<=w; j++) {
+		  for(int j = 1; j<4*w; j+=4) {
 			char c;
 			scanf(" %c", &c);
 			add(i, j, c);
 		  }
 	    }
-	    for(int i = 1; i<MAXN; i++) {
-			for(int j = 1; j<MAXN; j++) {
-				if(grid[i-1][j] == grid[i][j]) {
-					color[i][j] = color[i-1][j];
+	    for(int i = 0; i<=h; i++) {
+			for(int j = 0; j<=4*w; j++) {
+				if(color[i][j] == 0) {
+					if(grid[i][j] == 1) {
+						blacks.push_back(idx);
+					}
+					dfs(i, j, idx++);	
 				}
-				else if(grid[i][j-1] == grid[i][j]) {
-					color[i][j] = color[i][j-1];
-				}
-				else {
-					if(grid[i][j] == 1) blacks.push_back(idx);
-					color[i][j] = idx++;
-				}
+				
 			}
 		}
+		
+		/*for(int i = 0; i<=h+1; i++) {
+			for(int j = 0; j<=4*w+1; j++) {
+				printf("%d", color[i][j]);
+			}
+			printf("\n");
+		}*/
+		
 	    for(int i = 1; i<=h; i++) {
 		  for(int j = 1; j<=4*w; j++) {
 			if(grid[i][j] == 1) {
@@ -91,9 +109,10 @@ int main() {
 		  }
 	    }
 	    for(int i = 0; i<(int)blacks.size(); i++) {
-		  r[adj[blacks[i]].size()]++;
+			//printf(">>%d\n", adj[blacks[i]].size());
+			r[adj[blacks[i]].size()]++;
 	    }
-	    printf("Case: %d: ", teste++);
+	    printf("Case %d: ", teste++);
 	    for(int i = 0; i<r[3]; i++) printf("A");
 	    for(int i = 0; i<r[7]; i++) printf("D");
 	    for(int i = 0; i<r[5]; i++) printf("J");
