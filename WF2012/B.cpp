28985eb3914a69d678c1d6dae8f4df1a75e17546
ldmatos <ldmatos8@gmail.com>
2014-12-03 11:11:27 -0300

    problema B passado e problema D em desenvolvimento

--- WF2012/B.cpp
+++ WF2012/B.cpp
@@ -2,21 +2,30 @@
 
 using namespace std;
 
-#define MAXN 22
+//AC!
 
-int v[MAXN], r[MAXN];
+#define MAXN 23
+
+double v[MAXN], r[MAXN];
 double pi;
+double dif, maximum;
+
+double calc(double x) {
+	double ret = 0.0;
+	double mult = x;
+	for(int i = 0; i<MAXN; i++, mult*=x) {
+		ret += r[i]*mult;
+	}
+	return ret*pi;
+}
 
 double bs(double beg, double end, double expected) {
 	double mid;
 	for(int cnt = 0; cnt<100; cnt++) {
 		mid = (beg+end)/2;
-		double actual = 0.0;
-		for(int i = 0; i<MAXN; i++) {
-			actual += v[i]*mid;
-		}
-		actual *= pi;
-		if(actual > expected) end = mid;
+		double atual = 0.0;
+		atual = calc(mid) - dif;
+		if(atual > expected) end = mid;
 		else beg = mid;
 	}
 	return mid;
@@ -25,16 +34,40 @@ double bs(double beg, double end, double expected) {
 int main() {
 	
 	pi = acos(-1);
-	
 	int n;
-	scanf("%d", &n);
-	for(int i = 0; i<n; i++) {
-		scanf("%d", &v[i]);
-	}
-	for(int i = 0; i<n; i++) {
-		for(int j = 0; j<n; j++) {
-			r[i+j]+=v[i]*v[j];
+	double xLow, xHigh, inc;
+	int test = 1;
+	while(scanf("%d", &n) != EOF) {
+		for(int i = 0; i<=n; i++) {
+			scanf("%lf", &v[i]);
 		}
+		for(int i = 0; i<MAXN; i++) r[i] = 0;
+		scanf("%lf %lf %lf", &xLow, &xHigh, &inc);
+		for(int i = 0; i<=n; i++) {
+			for(int j = 0; j<=n; j++) {
+				r[i+j]+=v[i]*v[j];
+			}
+		}
+		for(int i = 1; i<=2*n; i++) {
+			r[i]/=(i+1);
+		}
+		dif = calc(xLow);
+		maximum = calc(xHigh) - dif;
+		printf("Case %d: %.2lf\n", test, maximum);
+		if(inc > maximum) {
+			printf("insufficient volume\n");
+		}
+		else {
+			double expected = inc;
+			for(int i = 0; i<8; i++, expected += inc) {
+				if(expected > maximum) break;
+				double mark = bs(xLow, xHigh, expected);
+				if(i == 0) printf("%.2lf", mark-xLow);
+				else printf(" %.2lf", mark-xLow);
+			}
+			printf("\n");
+		}
+		test++;
 	}
 	return 0;
 }
--- WF2012/D.cpp
+++ WF2012/D.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
-
+#include <cstring>
+#include <time.h>
 using namespace std;
 
-#define MAXN 110;
+#define MAX_N 100010
+#define MAXN 110
 
 struct p {
 	int q;
@@ -13,15 +15,46 @@ struct p {
 
 p dp[MAXN];
 
+string T, P;
+int b[MAX_N]; // b = back table, n = length of T, m = length of P
+
+void kmpPreprocess() { // call this before calling kmpSearch()
+  int i = 0, j = -1; b[0] = -1; // starting values
+  while (i < (int)P.size()) { // pre-process the pattern string P
+    while (j >= 0 && P[i] != P[j]) j = b[j]; // if different, reset j using b
+    i++; j++; // if same, advance both pointers
+    b[i] = j; // observe i = 8, 9, 10, 11, 12 with j = 0, 1, 2, 3, 4
+} }           // in the example of P = "SEVENTY SEVEN" above
+
+int kmpSearch() { // this is similar as kmpPreprocess(), but on string T
+  int ret = 0;
+  int i = 0, j = 0; // starting values
+  while (i < (int)T.size()) { // search through string T
+    while (j >= 0 && T[i] != P[j]) j = b[j]; // if different, reset j using b
+    i++; j++; // if same, advance both pointers
+    if (j == (int)P.size()) { // a match found when j == m
+	  ret++;
+      printf("P is found at index %d in T\n", i - j);
+      j = b[j]; // prepare j for the next possible match
+	}
+  }
+  return ret;
+}
+
 int main() {
 	int n;
-	string pat;
+	string P;
 	scanf("%d", &n);
-	cin >> pat;
-	dp[0].q = KMP();
+	cin >> P;
+	kmpPreprocess();
+	T = "0";
+	dp[0].q = kmpSearch();
 	dp[0].beg = "0";
 	dp[0].end = "1";
-	dp[1].q = KMP();
+	T = "1";
+	dp[1].q = kmpSearch();
 	dp[1].beg = "1";
 	dp[1].end = "1";
-	for(int i = 2; i<
+	printf("%d %d\n", dp[0].q, dp[1].q);
+	return 0;
+}
